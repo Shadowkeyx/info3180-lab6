951d9584df78ef6aec0a0172282d35eedffcb491
Bradley Henry <shadownine2010@hotmail.com>
2022-04-04 22:41:14 -0500

    Final Commit

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -2,13 +2,24 @@
     <ul class="news__list">
         <li v-for="article in articles" class="news__item">{{ article.title }}</li>
     </ul>
+
+    <form class="d-flex flex-column justify-content-center">
+        <div class="input-group mx-sm-3 mb-2">
+            <label class="visually-hidden" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here"/>
+            <button class="btn btn-primary mb-2">Search</button>
+            </div>
+            <p> You are searching for {{ searchTerm }}</p>
+            </form>
+
 </template>
 
 <script>
 export default {
     data() {
         return {
-            articles: []
+            articles: [],
+            searchTerm: ''
         }
     },
     created() {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script>
-import NewsList from '@/components/NewsList.vue'
+
 
 export default {
     data() {
@@ -7,7 +7,7 @@ export default {
             message: "Hello World! Welcome to VueJS. My name is Bradley."
         };
     },
-    components: { NewsList }
+
 }
 </script>
 
@@ -16,7 +16,7 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
-        <NewsList/>
+        
       </div>
     </div>
 </template>
